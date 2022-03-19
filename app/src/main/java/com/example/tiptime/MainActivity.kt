33d33b330f5c796f6c5ecc377c44b949af19c317
meame <meame69@gmail.com>
2022-03-19 18:47:26 +0200

    Latest stable version, this one actually calculates tips.

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -24,7 +24,11 @@ class MainActivity : AppCompatActivity() {
 
     fun calculateTip(){
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
+        if(cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
         val selectedId = binding.tipOptions.checkedRadioButtonId
         val tipPrecentage = when (selectedId){
             R.id.tip_twenty -> 0.20
